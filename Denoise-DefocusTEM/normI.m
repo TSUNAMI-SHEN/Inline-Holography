eee306bce281d1a21c8592bfe23f842e82828764
TSUNAMI-SHEN <74399586+TSUNAMI-SHEN@users.noreply.github.com>
2021-12-23 18:05:10 +0800

    Add files via upload

---
+++ Denoise-DefocusTEM/normI.m
@@ -0,0 +1,14 @@
+inputPath = './ML-Denoised/';
+output = './Norm-ML/';
+if ~exist(output, 'dir')
+    mkdir(output);
+end
+load('./sumstd-20211211.mat')
+n = 8;
+for n1=1:n
+    load([inputPath num2str(n1) '.mat'])
+    Fn1 = im2double(F);
+    Fn1 = Fn1 / sum(sum(Fn1)) * sumstd;
+    F = Fn1;
+    save([output num2str(n1) '.mat'],'F')
+end
