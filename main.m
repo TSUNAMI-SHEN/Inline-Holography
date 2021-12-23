2cbeaad9868ab250034b0dce2e9a96cd44f8715e
TSUNAMI-SHEN <74399586+TSUNAMI-SHEN@users.noreply.github.com>
2021-12-23 23:26:52 +0800

    Add files via upload

---
+++ main.m
@@ -0,0 +1,5 @@
+DMToMat('dm&txt', 'dm3', 'matlab');
+Preview('output', 0, 0.5)
+Autoalign('matlab', 'F', 0, 0.5, 500, 500, 100)
+Padding('F', 'output', 100)
+% GPU_RePhase_FRWR_edge_N('output', 'result', 300, -1, 0.09320271956, 10)
